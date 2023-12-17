ced5fdbaa83e513c685141ee1edf52587cc5cb21
Nishant <nishantdewangan2002@gmail.com>
2023-12-17 15:45:03 +0530

    final build 1.0

--- TastyMix/settings.py
+++ TastyMix/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2c$%4yjw&45_b!(m!rr&e#4wtz9d1_oiimrxhq4rme@50yob9k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://algovoyager.github.io/TastyMix/']
 
 
 # Application definition
