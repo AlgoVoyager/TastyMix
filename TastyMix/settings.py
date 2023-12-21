4030060f5ce46650ec3406c062aead4643d9fc1c
Nishant <nishantdewangan2002@gmail.com>
2023-12-21 19:04:49 +0530

    debug false

--- TastyMix/settings.py
+++ TastyMix/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2c$%4yjw&45_b!(m!rr&e#4wtz9d1_oiimrxhq4rme@50yob9k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['*']
 #
 # DEBUG = True
 #
